8a0137c3ff7e15e1ba4cbcae07fc37c2c5518bad
trongnguyen13 <trongnguyen13@yahoo.com>
2016-07-19 21:31:06 +0700

    add more charts

---
+++ RiskChart/scripts/charts/riskCombine.js
@@ -0,0 +1,41 @@
+﻿ko.bindingHandlers.combineChart = {
+    init: function (element, valueAccessor, allBindings) {
+        var options = ko.unwrap(valueAccessor()) || {};
+
+        var data = options.data() || [];
+        var name = options.name() || null;
+
+        $(element).highcharts({
+            title: {
+                text: 'Closed Risk'
+            },
+            xAxis: {
+                categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul']
+            },
+            labels: {
+                items: [{
+                    html: 'Total risk',
+                    style: {
+                        left: '50px',
+                        top: '18px',
+                        color: (Highcharts.theme && Highcharts.theme.textColor) || 'black'
+                    }
+                }]
+            },
+            series: [{
+                type: 'column',
+                name: 'Closed Risk',
+                data: [30, 20, 10, 13, 14, 28, 39]
+            }, {
+                type: 'spline',
+                name: 'Closed Issue',
+                data: [10, 15, 3, 6, 8, 14, 20],
+                marker: {
+                    lineWidth: 1,
+                    lineColor: Highcharts.getOptions().colors[3],
+                    fillColor: 'white'
+                }
+            }]
+        });
+    }
+}
---
+++ RiskChart/scripts/charts/riskStacked.js
@@ -0,0 +1,81 @@
+﻿ko.bindingHandlers.stackedChart = {
+    init: function (element, valueAccessor, allBindings) {
+        var options = ko.unwrap(valueAccessor()) || {};
+
+        var data = options.data() || [];
+        var name = options.name() || null;
+
+        $(element).highcharts({
+            chart: {
+                type: 'column'
+            },
+            title: {
+                //text: 'Total Rish By Category'
+                text: 'Open Rish'
+            },
+            xAxis: {
+                // categories: ['Category 1', 'Category 2', 'Category 3', 'Category 4', 'Category 5']
+                categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul']
+            },
+            yAxis: {
+                min: 0,
+                title: {
+                    text: 'Total risk'
+                },
+                stackLabels: {
+                    enabled: true,
+                    style: {
+                        fontWeight: 'bold',
+                        color: (Highcharts.theme && Highcharts.theme.textColor) || 'gray'
+                    }
+                }
+            },
+            legend: {
+                align: 'right',
+                x: -30,
+                verticalAlign: 'top',
+                y: 25,
+                floating: true,
+                backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || 'white',
+                borderColor: '#CCC',
+                borderWidth: 1,
+                shadow: false
+            },
+            tooltip: {
+                headerFormat: '<b>{point.x}</b><br/>',
+                pointFormat: '{series.name}: {point.y}<br/>Total: {point.stackTotal}'
+            },
+            plotOptions: {
+                column: {
+                    stacking: 'normal',
+                    dataLabels: {
+                        enabled: true,
+                        color: (Highcharts.theme && Highcharts.theme.dataLabelsColor) || 'white',
+                        style: {
+                            textShadow: '0 0 3px black'
+                        }
+                    }
+                }
+            },
+            credits: {
+                enabled: false
+            },
+            series: [{
+                name: 'Low',
+                color: 'rgb(124,252,0)',
+                //data: [5, 3, 4, 7, 2]
+                data: [5, 3, 4, 7, 2,4,5]
+            }, {
+                name: 'Medium',
+                color: 'yellow',
+                //data: [2, 2, 3, 2, 1]
+                data: [2, 2, 3, 2, 1,8,6]
+            }, {
+                name: 'High',
+                color: 'red',
+                //data: [3, 4, 4, 2, 5]
+                data: [3, 4, 4, 2, 5,14,5]
+            }]
+        });
+    }
+}
